78e01c6d50bee007d980cda6c425556c2d9fa0d2
Aktilek Korganbek <kalabaevbakitjan@gmail.com>
2026-09-24 10:35:16 +0500

    feat: add pagination and proper delete response

--- src/main/java/com/bloggingplatformapi/controller/PostController.java
+++ src/main/java/com/bloggingplatformapi/controller/PostController.java
@@ -5,46 +5,54 @@ import com.bloggingplatformapi.dto.PostResponse;
 import com.bloggingplatformapi.service.PostService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/posts")
 @RequiredArgsConstructor
 public class PostController {
+
     private final PostService postService;
 
     @GetMapping("/{id}")
-    public PostResponse getPostById(@PathVariable UUID id){
+    public PostResponse getPostById(@PathVariable UUID id) {
         return postService.getPostById(id);
     }
 
-    @PostMapping()
-    public PostResponse createPost(@Valid @RequestBody PostRequest request){
+    @PostMapping
+    public PostResponse createPost(@Valid @RequestBody PostRequest request) {
         return postService.createPost(request);
     }
+
     @PutMapping("/{id}")
-    public PostResponse updatePost(@PathVariable UUID id,
-                                   @Valid @RequestBody PostRequest request){
+    public PostResponse updatePost(
+            @PathVariable UUID id,
+            @Valid @RequestBody PostRequest request
+    ) {
         return postService.updatePost(id, request);
     }
 
     @DeleteMapping("/{id}")
-    public void deletePostById(@PathVariable UUID id){
+    public ResponseEntity<Void> deletePostById(@PathVariable UUID id) {
         postService.deletePostById(id);
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping
-    public List<PostResponse> getPosts(
-            @RequestParam(required = false) String term
-    ){
-        if (term != null && !term.isBlank()){
-            return postService.searchPosts(term);
+    public Page<PostResponse> getPosts(
+            @RequestParam(required = false) String term,
+            @PageableDefault(size = 10, sort = "createdAt") Pageable pageable
+    ) {
+        if (term != null && !term.isBlank()) {
+            return postService.searchPosts(term, pageable);
         }
 
-        return postService.getAllPosts();
+        return postService.getAllPosts(pageable);
     }
-
 }
