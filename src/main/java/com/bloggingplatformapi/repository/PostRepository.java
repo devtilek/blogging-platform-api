8c473001ebc9e97fafe0a31009489f5071f02afc
Aktilek Korganbek <kalabaevbakitjan@gmail.com>
2026-09-24 10:34:57 +0500

    feat: add pageable post search

--- src/main/java/com/bloggingplatformapi/repository/PostRepository.java
+++ src/main/java/com/bloggingplatformapi/repository/PostRepository.java
@@ -1,11 +1,12 @@
 package com.bloggingplatformapi.repository;
 
 import com.bloggingplatformapi.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface PostRepository extends JpaRepository<Post, UUID> {
@@ -16,5 +17,5 @@ public interface PostRepository extends JpaRepository<Post, UUID> {
                OR LOWER(p.content) LIKE LOWER(CONCAT('%', :term, '%'))
                OR LOWER(p.category) LIKE LOWER(CONCAT('%', :term, '%'))
             """)
-    List<Post> searchPost(@Param("term") String term);
+    Page<Post> searchPosts(@Param("term") String term, Pageable pageable);
 }
