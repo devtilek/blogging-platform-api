81b8feffaa914f1b3786b531d5abe90501d5c820
Aktilek Korganbek <kalabaevbakitjan@gmail.com>
2026-09-24 10:35:03 +0500

    refactor: expose pageable post service operations

--- src/main/java/com/bloggingplatformapi/service/PostService.java
+++ src/main/java/com/bloggingplatformapi/service/PostService.java
@@ -2,16 +2,22 @@ package com.bloggingplatformapi.service;
 
 import com.bloggingplatformapi.dto.PostRequest;
 import com.bloggingplatformapi.dto.PostResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface PostService {
 
     PostResponse getPostById(UUID id);
+
     PostResponse createPost(PostRequest request);
-    List<PostResponse> getAllPosts();
+
+    Page<PostResponse> getAllPosts(Pageable pageable);
+
     PostResponse updatePost(UUID id, PostRequest request);
+
     void deletePostById(UUID id);
-    List<PostResponse> searchPosts(String term);
+
+    Page<PostResponse> searchPosts(String term, Pageable pageable);
 }
